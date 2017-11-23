33226b0a88846fcc3688d84fc952e267c1c352e2
somtochukwu-nwaogazie <somto.nwaogazie@gmail.com>
2017-11-23 23:42:56 +0100

    feature(API-Endpoint):Create endpoint to edit an event

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ import Center from './controller/centers';
 const app = express();
 
 app.use(bodyParser.json());
-app.post('/events/', Events.addEvents);
-app.get('/events/', Events.getEvents);
+app.post('/events/', Events.addEvent);
+app.get('/events/', Events.getEvent);
 app.post('/center/', Center.addCenter);
 app.get('/center/', Center.getCenter);
+app.put('/events/:eventId', Events.editEvent);
 
-app.listen(5000, () => console.log('Example app listening on port 3000!'));
+app.listen(5000, () => console.log('Example app listening on port 5000!'));
 
